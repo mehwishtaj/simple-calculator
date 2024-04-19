de8e566dae992698a45294d1c88b2bbb2825d3e1
mehwishtaj <konain786yaseen@gmail.com>
2024-04-20 02:37:45 +0500

    Adding a simple CLI Calculator project

---
+++ main.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//printing a Welcome message
+console.log("\n\tWelcome To \'CodewithMehwish\' - CLI Simple Calculator\n");
+
+// Asking Question from users through inquirer
+
+let answers = await inquirer.prompt([
+
+   {message: "Enter First Number",type: "number", name: "firstNumber"},
+{message: "Enter Second Number",type: "number", name: "secondNumber"},
+{
+message: "Select one operator to perform operations",
+type: "list",
+name: "operator",
+choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+},
+]);
+
+// conditional statements if-Else
+
+if(answers.operator === "Addition"){
+   console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtractions"){
+   console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multiplication"){
+   console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+   console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+   console.log("Invalid Input")
+}
